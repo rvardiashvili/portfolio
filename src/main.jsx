9fe81c1083ff1e5d88647d4177f002466d6202f7
rvardiashvili <vardiashvilirati33@gmail.com>
2025-11-23 21:25:25 +0400

    fix: Ensure HashRouter functionality with stable react-router-dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 
 // Set favicon dynamically to the profile picture
 const link = document.createElement('link');
 link.rel = 'icon';
-link.href = '/media/pfp.png'; // Note: In Vite, public assets are at root
+link.href = '/portfolio/media/pfp.png'; 
 document.head.appendChild(link);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/portfolio">
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 )
